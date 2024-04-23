329cb09d327b0e02e8969255c3c97c79545460de
alupoaiemircea <mircea@axiologic.net>
2024-04-23 17:23:41 +0300

    moved user module impl to server

--- modules/document/apis/paragraph.js
+++ modules/document/apis/paragraph.js
@@ -1,3 +1,21 @@
+async function sendRequest(url, method, data){
+    let result;
+    let init = {
+        method: method
+    };
+    if(method === "POST" || method === "PUT"){
+        init.body = typeof data === "string" ? data : JSON.stringify(data);
+        init.headers = {
+            "Content-type": "application/json; charset=UTF-8"
+        };
+    }
+    try {
+        result = await fetch(url,init);
+    } catch (err) {
+        console.error(err);
+    }
+    return await result.text();
+}
 async function addParagraph(spaceId, documentId, chapterId, paragraphData){
     let paragraphObj ={
         documentId: documentId,
--- modules/user/index.js
+++ modules/user/index.js
@@ -6,43 +6,6 @@ const apiModules = {
     }
 };
 
-const dataModules = {
-    templates: {
-        get defaultApiKeyTemplate() {
-            const data = require('./data/templates/json/defaultApiKeyTemplate.json');
-            Object.defineProperty(dataModules, 'defaultApiKeyTemplate', {
-                value: data,
-                writable: false,
-                configurable: true
-            });
-            return data;
-        },
-        get defaultUserTemplate() {
-            const data = require('./data/templates/json/defaultUserTemplate.json');
-            Object.defineProperty(dataModules, 'defaultUserTemplate', {
-                value: data,
-                writable: false,
-                configurable: true
-            });
-            return data;
-        },
-        get demoUser() {
-            const data = require('./data/templates/json/demoUser.json');
-            Object.defineProperty(dataModules, 'demoUser', {value: data, writable: false, configurable: true});
-            return data;
-        },
-        get userRegistrationTemplate() {
-            const data = require('./data/templates/json/userRegistrationTemplate.json');
-            Object.defineProperty(dataModules, 'userRegistrationTemplate', {
-                value: data,
-                writable: false,
-                configurable: true
-            });
-            return data;
-        }
-    }
-};
-
 function loadAPIs(...apiNames) {
     if (apiNames.length === 0) {
         apiNames = Object.keys(apiModules);
@@ -63,26 +26,4 @@ function loadAPIs(...apiNames) {
     }
     return selectedApis;
 }
-
-function loadData(...dataTypes) {
-    if (dataTypes.length === 0) {
-        dataTypes = Object.keys(dataModules);
-    }
-    if (dataTypes.length === 1) {
-        const data = dataModules[dataTypes[0]];
-        if (!data) {
-            throw new Error(`Data '${dataTypes[0]}' not found`);
-        }
-        return data;
-    }
-    const selectedData = {};
-    for (const type of dataTypes) {
-        if (!dataModules[type]) {
-            throw new Error(`Data '${type}' not found`);
-        }
-        selectedData[type] = dataModules[type];
-    }
-    return selectedData;
-}
-
-module.exports = { loadAPIs, loadData };
+module.exports = { loadAPIs };
