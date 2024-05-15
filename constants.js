e90a821a936860394ef9c088cd3dc65123fda525
alupoaiemircea <mircea@axiologic.net>
2024-05-15 16:56:46 +0300

    css colors cleanup

--- constants.js
+++ constants.js
@@ -1,3 +1,7 @@
 module.exports = {
-    DEFAULT_ID_LENGTH: 16
+    DEFAULT_ID_LENGTH: 16,
+    PERSONALITIES : {
+        "DEFAULT_PERSONALITY_NAME" : "Assistant",
+        "DEFAULT_PERSONALITY_ID" : "2idYvpTEKXM5",
+    }
 }
--- modules/flow/index.js
+++ modules/flow/index.js
@@ -1,5 +1,6 @@
+const IFlow = require("./models/IFlow.js");
 const sendRequest = require("assistos").loadModule("util").sendRequest;
-
+const constants = require("../../constants.js");
 async function loadFlows(spaceId){
     return await import(`/spaces/flows/${spaceId}`);
 }
@@ -15,12 +16,72 @@ async function updateFlow(spaceId, flowName, flowData){
 async function deleteFlow(spaceId, flowName){
     return await sendRequest(`/spaces/flows/${spaceId}/${flowName}`, "DELETE")
 }
-const IFlow = require("./models/IFlow.js");
+async function callFlow(flowName, context, personalityId) {
+    let flowClass;
+    if (assistOS.currentApplicationName === assistOS.configuration.defaultApplicationName) {
+        flowClass = await assistOS.space.getFlow(flowName);
+    } else {
+        let app = assistOS.space.getApplicationByName(assistOS.currentApplicationName);
+        flowClass = app.getFlow(flowName);
+    }
+    let personality;
+    if (personalityId) {
+        personality = assistOS.space.getPersonality(personalityId);
+    } else {
+        personality = assistOS.space.getPersonality(constants.PERSONALITIES.DEFAULT_PERSONALITY_ID);
+    }
+    if (flowClass.inputSchema) {
+        // assistOS.services.validateSchema(context, flow.inputSchema, "input");
+    }
+    let flowInstance = new flowClass();
+    if (flowInstance.start === undefined) {
+        throw new Error(`Flow ${flowInstance.constructor.name} must have a function named 'start'`);
+    }
+    const apis = Object.getOwnPropertyNames(IFlow.prototype)
+        .filter(method => method !== 'constructor');
+    apis.forEach(methodName => {
+        flowInstance[methodName] = IFlow.prototype[methodName].bind(flowInstance);
+    });
+    if (flowClass.inputSchema) {
+        // assistOS.services.validateSchema(context, flow.inputSchema, "input");
+    }
+
+    let response;
+    try {
+        response = await ((context, personality) => {
+            let returnPromise = new Promise((resolve, reject) => {
+                flowInstance.resolve = resolve;
+                flowInstance.reject = reject;
+            });
+            flowInstance.personality = personality;
+            flowInstance.start(context, personality);
+            return returnPromise;
+        })(context, personality);
+    } catch (e) {
+        console.error(e);
+        return await showApplicationError("Flow execution Error", `Error executing flow ${flowObj.flowInstance.constructor.name}`, e);
+    }
+    if (flowClass.outputSchema) {
+        if (typeof flowClass.outputSchema.isValid === "undefined") {
+            try {
+                let parsedResponse = JSON.parse(response);
+                //assistOS.services.validateSchema(parsedResponse, flowObj.flowClass.outputSchema, "output");
+                return parsedResponse;
+            } catch (e) {
+                console.error(e);
+                return await showApplicationError(e, e, e.stack);
+            }
+        }
+    }
+    return response;
+}
+
 module.exports = {
     getFlow,
     loadFlows,
     addFlow,
     updateFlow,
     deleteFlow,
+    callFlow,
     IFlow
 }
