e2f54ebe2a7e09458b0292650838523f74434e41
alupoaiemircea <mircea@axiologic.net>
2024-07-31 11:19:54 +0300

    update tts commands

--- constants.js
+++ constants.js
@@ -1,10 +1,10 @@
 module.exports = {
     DEFAULT_ID_LENGTH: 16,
-    PERSONALITIES : {
-        DEFAULT_PERSONALITY_NAME : "Assistant",
-        DEFAULT_PERSONALITY_ID : "2idYvpTEKXM5",
+    PERSONALITIES: {
+        DEFAULT_PERSONALITY_NAME: "Assistant",
+        DEFAULT_PERSONALITY_ID: "2idYvpTEKXM5",
     },
-    ENV_TYPE:{
+    ENV_TYPE: {
         NODE: "node",
         BROWSER: "browser",
         UNKNOWN: "unknown"
@@ -12,4 +12,63 @@ module.exports = {
     ENVIRONMENT_MODE: "DEVELOPMENT_BASE_URL",
     PRODUCTION_BASE_URL: "http://demo.assistos.net:8080",
     DEVELOPMENT_BASE_URL: "http://localhost:8080",
+    TTS_COMMANDS: [
+        {
+            NAME: "!speech",
+            EXECUTE_FN: "textToSpeech",
+            PARAMETERS: [{
+                NAME: "personalityName",
+                TYPE: "string",
+                REQUIRED: true
+            }, {
+                NAME: "emotion",
+                TYPE: "string",
+                VALUES: ['female_happy',
+                    'female_sad',
+                    'female_angry',
+                    'female_fearful',
+                    'female_disgust',
+                    'female_surprised',
+                    'male_happy',
+                    'male_sad',
+                    'male_angry',
+                    'male_fearful',
+                    'male_disgust',
+                    'male_surprised'],
+                REQUIRED: true
+            },
+            {
+                NAME: "styleGuidance",
+                TYPE: "number",
+                MIN_VALUE: 0,
+                MAX_VALUE: 100,
+                REQUIRED: false
+            }, {
+                NAME: "temperature",
+                TYPE: "number",
+                MIN_VALUE: 0,
+                MAX_VALUE: 100,
+                REQUIRED: false
+            }, {
+                NAME: "voiceGuidance",
+                TYPE: "number",
+                MIN_VALUE: 0,
+                MAX_VALUE: 100,
+                REQUIRED: false
+            }
+            ]
+        },
+        {
+            NAME: "!silence",
+            EXECUTE_FN: "addPauseInAudio",
+            PARAMETERS: [{
+                NAME:"duration",
+                TYPE:"number",
+                MIN_VALUE:0,
+                MAX_VALUE:100,
+                REQUIRED: true
+            }]
+            //duration is in seconds
+        }
+    ]
 }
