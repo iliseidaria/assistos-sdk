4579bb20554af482c838faef4b7baaa7c0053a43
alupoaiemircea <mircea@axiologic.net>
2025-03-06 16:14:55 +0200

    moved changes to new branch

--- modules/user/index.js
+++ modules/user/index.js
@@ -1,115 +1,10 @@
-const crypto = require('opendsu').loadAPI('crypto');
 const {request} = require("../util");
-const constants = require("../../constants");
 async function sendRequest(url, method, data) {
     return await request(url, method, this.__securityContext, data);
-}
-const prepareSecret = (secret) => {
-    return Array.from(crypto.sha256JOSE(secret))
-        .map(b => b.toString(16).padStart(2, '0'))
-        .join('');
-}
-
-async function registerUser(email, password, imageId, inviteToken) {
-    return await this.sendRequest(`/users`, "POST",{
-        email: email,
-        password: prepareSecret(password),
-        inviteToken: inviteToken,
-        imageId: imageId
-    });
-}
-
-async function generateVerificationCode(email,password){
-    const headers = {
-        "Content-Type": "application/json; charset=UTF-8",
-    };
-    const options = {
-        method: "POST",
-        headers: headers,
-        body: JSON.stringify({
-            email: email,
-            password: prepareSecret(password)
-        })
-    };
-    const response = await fetch(`/users/password-reset/request`, options);
-    if (!response.ok) {
-        const error = new Error(`HTTP error! status: ${response.status}, message: ${response.message}`);
-        error.statusCode = response.status;
-        throw error;
-    }
-
-    return await response.json();
-}
-async function resetPassword(email, password, code) {
-    const headers = {
-        "Content-Type": "application/json; charset=UTF-8",
-    };
-    const options = {
-        method: "POST",
-        headers: headers,
-        body: JSON.stringify({
-            email: email,
-            password: prepareSecret(password),
-            code: code
-        })
-    };
-    const response = await fetch(`/users/password-reset/verify`, options);
-    if (!response.ok) {
-        const error = new Error(`HTTP error! status: ${response.status}, message: ${response.message}`);
-        error.statusCode = response.status;
-        throw error;
-    }
-    return await response.json();
-}
-
-async function activateUser(activationToken) {
-    const headers = {
-        "Content-Type": "application/json; charset=UTF-8",
-    };
-    const options = {
-        method: "POST",
-        headers: headers,
-        body: JSON.stringify({
-            activationToken: activationToken
-        })
-    };
-    const response = await fetch(`/users/verify`, options);
-
-    if (!response.ok) {
-        throw new Error(`HTTP error! status: ${response.status}, message: ${response.message}`);
-    }
-
-    return await response.json();
-}
-
-async function loginUser(email, password) {
-    const headers = {
-        "Content-Type": "application/json; charset=UTF-8",
-    };
-    const options = {
-        method: "POST",
-        headers: headers,
-        body: JSON.stringify({
-            email: email,
-            password: prepareSecret(password)
-        })
-    };
-    const response = await fetch(`/users/login`, options);
-    if (!response.ok) {
-        const error = new Error(`HTTP error! status: ${response.status}, message: ${response.message}`);
-        error.statusCode = response.status;
-        throw error;
-    }
-
-    return await response.json();
 }
 
 async function loadUser() {
     return await this.sendRequest(`/users`, "GET");
-}
-
-async function logoutUser() {
-    return await this.sendRequest(`/users/logout`, "GET");
 }
 
 async function deleteAPIKey(spaceId, type) {
@@ -125,8 +20,8 @@ async function getUserProfileImage(userId) {
 async function updateUserImage(userId, imageId) {
     return await this.sendRequest(`/users/profileImage/${userId}`, "POST", {imageId});
 }
-async function logout(){
-    let response = await fetch(`/auth/logout`);
+async function logoutUser(){
+    let response = await fetch(`/users/logout`);
     return response.ok;
 }
 async function accountExists(email){
@@ -134,7 +29,7 @@ async function accountExists(email){
     let response = await fetch(`/auth/accountExists/${email}`);
     return await response.json();
 }
-async function login(email, code, createSpace){
+async function loginUser(email, code, createSpace){
     let response = await fetch(`/users/login`, {
         method: 'POST',
         headers: {
@@ -155,20 +50,14 @@ async function generateAuthCode(email, refererId){
     return await response.json();
 }
 module.exports = {
-    registerUser,
-    activateUser,
-    loginUser,
     loadUser,
-    logoutUser,
     editAPIKey,
     deleteAPIKey,
     sendRequest,
-    generateVerificationCode,
-    resetPassword,
     getUserProfileImage,
     updateUserImage,
-    logout,
+    logoutUser,
     accountExists,
-    login,
+    loginUser,
     generateAuthCode
 }
