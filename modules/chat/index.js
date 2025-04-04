39a8562cb6d164967e2ce21f4495e1bce001d6a6
VictorCondurat <conduratvictor@yahoo.com>
2025-04-04 20:21:30 +0300

    feat(WebAssistant): implement persistence for web assistant configuration and themes

--- modules/chat/index.js
+++ modules/chat/index.js
@@ -1,96 +1,66 @@
-const {getAPIClient} = require("../util/utils");
-const constants = require("../../constants");
+const { getAPIClient } = require("../util/utils");
+const { CHAT_PLUGIN } = require("../../constants");
 
-async function getChat(spaceId, chatId) {
-    let client = await getAPIClient("*", constants.CHAT_PLUGIN, spaceId);
-    return await client.getChat(chatId);
-}
-async function getChatMessages(spaceId, chatId) {
-    let client = await getAPIClient("*", constants. CHAT_PLUGIN, spaceId);
-    return await client.getChatMessages(chatId);
-}
-async function getChatContext(spaceId, chatId) {
-    let client = await getAPIClient("*", constants. CHAT_PLUGIN, spaceId);
-    return await client.getChatContext(chatId);
-}
-async function createChat(spaceId, personalityId) {
-    let client = await getAPIClient("*", constants. CHAT_PLUGIN, spaceId);
-    return await client.createChat(spaceId, personalityId);
-}
-async function deleteChat(spaceId, chatId) {
-    let client = await getAPIClient("*", constants. CHAT_PLUGIN, spaceId);
-    return await client.deleteChat(chatId);
-}
-async function resetChat(spaceId, chatId) {
-    let client = await getAPIClient("*", constants. CHAT_PLUGIN, spaceId);
-    return await client.resetChat(chatId);
-}
-async function resetChatContext(spaceId, chatId) {
-    let client = await getAPIClient("*", constants. CHAT_PLUGIN, spaceId);
-    return await client.resetChatContext(chatId);
-}
-async function resetChatMessages(spaceId, chatId) {
-    let client = await getAPIClient("*", constants. CHAT_PLUGIN, spaceId);
-    return await client.resetChatMessages(chatId);
-}
-async function addPreferenceToContext(spaceId, chatId, preference) {
-    let client = await getAPIClient("*", constants. CHAT_PLUGIN, spaceId);
-    return await client.addPreferenceToContext(chatId, preference);
-}
-async function deletePreferenceFromContext(spaceId, chatId, preference) {
-    let client = await getAPIClient("*", constants. CHAT_PLUGIN, spaceId);
-    return await client.deletePreferenceFromContext(chatId, preference);
-}
-async function addMessageToContext(spaceId, chatId, messageId) {
-    let client = await getAPIClient("*", constants. CHAT_PLUGIN, spaceId);
-    return await client.addMessageToContext(chatId, messageId);
-}
-async function removeMessageFromContext(spaceId, chatId, messageId) {
-    let client = await getAPIClient("*", constants. CHAT_PLUGIN, spaceId);
-    return await client.removeMessageFromContext(chatId, messageId);
-}
-async function updateChatContextItem(spaceId, chatId, contextItemId, contextItem) {
-    let client = await getAPIClient("*", constants. CHAT_PLUGIN, spaceId);
-    return await client.updateChatContextItem(chatId, contextItemId, contextItem);
-}
-async function addChatToAgent(spaceId, chatId, agentId) {
-    let client = await getAPIClient("*", constants. CHAT_PLUGIN, spaceId);
-    return await client.addChatToAgent(chatId, agentId);
-}
-async function removeChatFromAgent(spaceId, chatId, agentId) {
-    let client = await getAPIClient("*", constants. CHAT_PLUGIN, spaceId);
-    return await client.removeChatFromAgent(chatId, agentId);
-}
-async function sendMessage(spaceId,chatId,userId,message,role){
-    let client = await getAPIClient("*", constants. CHAT_PLUGIN, spaceId);
-    return await client.sendMessage(chatId,userId,message,role);
-}
-async function sendQuery(spaceId,chatId,personalityId,userId,prompt){
-    let client = await getAPIClient("*", constants. CHAT_PLUGIN, spaceId);
-    return await client.sendQuery(chatId, personalityId,userId,prompt);
-}
-async function sendQueryStreaming(spaceId,chatId, personalityId, userId, prompt) {
-    let client = await getAPIClient("*", constants. CHAT_PLUGIN, spaceId);
-    return await client.sendQueryStreaming(chatId, personalityId,userId,prompt);
+function withClient(fn) {
+    return async function (spaceId, ...args) {
+        const client = await getAPIClient("*", CHAT_PLUGIN, spaceId);
+        return await fn.call(this, client, spaceId, ...args);
+    };
 }
 
 module.exports = {
-    getChat,
-    getChatMessages,
-    getChatContext,
-    createChat,
-    deleteChat,
-    resetChat,
-    resetChatContext,
-    resetChatMessages,
-    addPreferenceToContext,
-    deletePreferenceFromContext,
-    addMessageToContext,
-    removeMessageFromContext,
-    updateChatContextItem,
-    addChatToAgent,
-    removeChatFromAgent,
-    sendMessage,
-    sendQuery,
-    sendQueryStreaming
-}
+    getChat: withClient(function (client, spaceId, chatId) {
+        return client.getChat(chatId);
+    }),
+    getChatMessages: withClient(function (client, spaceId, chatId) {
+        return client.getChatMessages(chatId);
+    }),
+    getChatContext: withClient(function (client, spaceId, chatId) {
+        return client.getChatContext(chatId);
+    }),
+    createChat: withClient(function (client, spaceId, personalityId) {
+        return client.createChat(spaceId, personalityId);
+    }),
+    deleteChat: withClient(function (client, spaceId, chatId) {
+        return client.deleteChat(chatId);
+    }),
+    resetChat: withClient(function (client, spaceId, chatId) {
+        return client.resetChat(chatId);
+    }),
+    resetChatContext: withClient(function (client, spaceId, chatId) {
+        return client.resetChatContext(chatId);
+    }),
+    resetChatMessages: withClient(function (client, spaceId, chatId) {
+        return client.resetChatMessages(chatId);
+    }),
+    addPreferenceToContext: withClient(function (client, spaceId, chatId, preference) {
+        return client.addPreferenceToContext(chatId, preference);
+    }),
+    deletePreferenceFromContext: withClient(function (client, spaceId, chatId, preference) {
+        return client.deletePreferenceFromContext(chatId, preference);
+    }),
+    addMessageToContext: withClient(function (client, spaceId, chatId, messageId) {
+        return client.addMessageToContext(chatId, messageId);
+    }),
+    removeMessageFromContext: withClient(function (client, spaceId, chatId, messageId) {
+        return client.removeMessageFromContext(chatId, messageId);
+    }),
+    updateChatContextItem: withClient(function (client, spaceId, chatId, contextItemId, contextItem) {
+        return client.updateChatContextItem(chatId, contextItemId, contextItem);
+    }),
+    addChatToAgent: withClient(function (client, spaceId, chatId, agentId) {
+        return client.addChatToAgent(chatId, agentId);
+    }),
+    removeChatFromAgent: withClient(function (client, spaceId, chatId, agentId) {
+        return client.removeChatFromAgent(chatId, agentId);
+    }),
+    sendMessage: withClient(function (client, spaceId, chatId, userId, message, role) {
+        return client.sendMessage(chatId, userId, message, role);
+    }),
+    sendQuery: withClient(function (client, spaceId, chatId, personalityId, userId, prompt) {
+        return client.sendQuery(chatId, personalityId, userId, prompt);
+    }),
+    sendQueryStreaming: withClient(function (client, spaceId, chatId, personalityId, userId, prompt) {
+        return client.sendQueryStreaming(chatId, personalityId, userId, prompt);
+    })
+};
